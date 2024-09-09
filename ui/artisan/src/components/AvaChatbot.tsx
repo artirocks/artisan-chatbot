668a127aef1aa5635cb6c06e37e3e3db9abccb9b
Arti Kumari <k.artiism06@gmail.com>
2024-09-09 17:35:54 +0530

    Enhanced Chatbot Edit functionality: Show response for updatedMessage

--- ui/artisan/src/components/AvaChatbot.tsx
+++ ui/artisan/src/components/AvaChatbot.tsx
@@ -12,8 +12,20 @@ import {
   Text,
   VStack,
   Divider,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  useEditableControls,
+  ButtonGroup,
 } from "@chakra-ui/react";
-import { DeleteIcon, EditIcon, CloseIcon } from "@chakra-ui/icons";
+import {
+  DeleteIcon,
+  EditIcon,
+  ChevronDownIcon,
+  CloseIcon,
+  CheckIcon,
+} from "@chakra-ui/icons";
 import { TfiArrowsCorner } from "react-icons/tfi";
 import { CgWebsite } from "react-icons/cg";
 
@@ -28,9 +40,12 @@ type Message = {
 const AvaChatbot: React.FC = () => {
   const [messages, setMessages] = useState<Message[]>([]);
   const [inputValue, setInputValue] = useState<string>("");
-  const [isChatOpen, setIsChatOpen] = useState<boolean>(true); // State to handle the chat window visibility
+  const [isChatOpen, setIsChatOpen] = useState<boolean>(true);
+  const [editingMessage, setEditingMessage] = useState<number | null>(null);
+  const [editingMessageId, setEditingMessageId] = useState<number | null>(null);
+  const [editingText, setEditingText] = useState<string>("");
 
-  const messageContainerRef = useRef<HTMLDivElement | null>(null);
+  const messageContainerRef = useRef<HTMLDivElement>();
 
   const handleSendMessage = async () => {
     const userMessage: Message = {
@@ -53,20 +68,41 @@ const AvaChatbot: React.FC = () => {
     const botMessage: Message = {
       id: Date.now() + 1,
       text: data.generated_text,
-      user: "Ava",
+      user: "Jane",
       isBot: true,
     };
 
     setMessages((prevMessages) => [...prevMessages, botMessage]);
     setInputValue("");
   };
+  const handleEditMessage = async (id: number, newText: string) => {
+    const updatedMessage: Message = {
+      id: id,
+      text: newText,
+      user: "Jane",
+      isBot: false,
+    };
+    setMessages((prevMessages) => [...prevMessages, updatedMessage]);
 
-  const handleEditMessage = (id: number, newText: string) => {
-    setMessages((prevMessages) =>
-      prevMessages.map((msg) =>
-        msg.id === id ? { ...msg, text: newText } : msg
-      )
-    );
+    const response = await fetch("http://localhost:8000/bot/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ prompt: newText }),
+    });
+    const data = await response.json();
+
+    const botMessage: Message = {
+      id: Date.now(),
+      text: data.generated_text,
+      user: "Ava",
+      isBot: true,
+    };
+
+    setMessages((prevMessages) => [...prevMessages, botMessage]);
+
+    setEditingMessageId(null);
   };
 
   const handleDeleteMessage = (id: number) => {
@@ -79,6 +115,38 @@ const AvaChatbot: React.FC = () => {
     );
   };
 
+  const startEditingMessage = (id: number, text: string) => {
+    setEditingMessageId(id);
+    setEditingText(text); // Set the current text in the input for editing
+  };
+
+  const submitEditedMessage = (id: number) => {
+    if (editingText.trim()) {
+      handleEditMessage(id, editingText);
+    }
+  };
+
+  const EditableControls = ({ id }: { id: number }) => {
+    const { isEditing, getSubmitButtonProps, getCancelButtonProps } =
+      useEditableControls();
+
+    return isEditing ? (
+      <ButtonGroup justifyContent="center" size="sm" mt="2">
+        <IconButton
+          icon={<CheckIcon />}
+          {...getSubmitButtonProps()}
+          onClick={() => submitEditedMessage(id)}
+          aria-label="Submit"
+        />
+        <IconButton
+          icon={<CloseIcon />}
+          {...getCancelButtonProps()}
+          onClick={() => setEditingMessageId(null)}
+          aria-label="Cancel"
+        />
+      </ButtonGroup>
+    ) : null;
+  };
   useEffect(() => {
     if (messageContainerRef.current) {
       messageContainerRef.current.scrollTop =
@@ -92,7 +160,7 @@ const AvaChatbot: React.FC = () => {
     <Box position="fixed" bottom="20px" right="20px">
       <Box
         width="400px"
-        height="620px"
+        height="640px"
         p="4"
         bg="gray.50"
         borderRadius="lg"
@@ -113,13 +181,6 @@ const AvaChatbot: React.FC = () => {
             variant="ghost"
             aria-label="Open dashboard"
           />
-          {/* <IconButton
-            size="sm"
-            icon={<CloseIcon />}
-            onClick={() => setIsChatOpen(false)}
-            variant="ghost"
-            aria-label="Close chat"
-          /> */}
 
           <VStack alignItems="center">
             <Avatar
@@ -128,8 +189,9 @@ const AvaChatbot: React.FC = () => {
               src="https://www.artisan.co/assets/ava.svg"
             />
             <Text ml="2" fontWeight="bold">
-              I am Ava, How can I help you?
+              Hey, 👋 I am Ava
             </Text>
+            <Text ml="2">Ask me anything or pick a place to start</Text>
           </VStack>
         </Box>
         <Divider mt={7} />
@@ -152,6 +214,7 @@ const AvaChatbot: React.FC = () => {
               alignItems="center"
               justifyContent={message.isBot ? "flex-start" : "flex-end"}
               mb="2"
+              position="relative"
             >
               {message.isBot && (
                 <Avatar
@@ -168,42 +231,59 @@ const AvaChatbot: React.FC = () => {
                 position="relative"
                 wordWrap="break-word"
                 whiteSpace="pre-wrap"
+                onMouseEnter={() => setEditingMessage(message.id)}
+                onMouseLeave={() => setEditingMessage(null)}
               >
                 <Editable
-                  value={message.text}
-                  isDisabled={message.isDeleted}
+                  value={
+                    editingMessageId === message.id ? editingText : message.text
+                  }
+                  isDisabled={
+                    message.isDeleted || editingMessageId !== message.id
+                  }
                   onSubmit={(newText) => handleEditMessage(message.id, newText)}
+                  onChange={setEditingText}
+                  onFocus={() => setEditingMessageId(message.id)}
                 >
-                  <EditablePreview />
+                  <EditablePreview width="auto" />
                   <EditableInput />
+                  {editingMessageId === message.id && !message.isBot && (
+                    <EditableControls id={message.id} />
+                  )}
                 </Editable>
 
-                {!message.isBot && !message.isDeleted && (
-                  <Flex
-                    position="absolute"
-                    top="0"
-                    right="-30px"
-                    direction="column"
-                  >
-                    <IconButton
-                      size="sm"
-                      icon={<EditIcon />}
-                      variant="ghost"
-                      colorScheme="blue"
-                      aria-label="Edit message"
-                    />
-
-                    <IconButton
-                      size="sm"
-                      icon={<DeleteIcon />}
-                      onClick={() => handleDeleteMessage(message.id)}
-                      variant="ghost"
-                      colorScheme="red"
-                      mt="2"
-                      aria-label="Delete message"
-                    />
-                  </Flex>
-                )}
+                {editingMessage === message.id &&
+                  !message.isBot &&
+                  !message.isDeleted && (
+                    <Menu>
+                      <MenuButton
+                        as={IconButton}
+                        icon={<ChevronDownIcon />}
+                        variant="ghost"
+                        aria-label="More options"
+                        size="sm"
+                        position="absolute"
+                        top="0"
+                        right="0"
+                      />
+                      <MenuList>
+                        <MenuItem
+                          icon={<EditIcon />}
+                          onClick={() =>
+                            startEditingMessage(message.id, message.text)
+                          }
+                        >
+                          Edit
+                        </MenuItem>
+                        <MenuItem
+                          icon={<DeleteIcon />}
+                          onClick={() => handleDeleteMessage(message.id)}
+                        >
+                          Delete
+                        </MenuItem>
+                      </MenuList>
+                    </Menu>
+                  )}
               </Box>
             </Flex>
           ))}
